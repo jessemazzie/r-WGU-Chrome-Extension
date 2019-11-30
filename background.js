7f3c39d07cee976a52c22ab3b41a0cafaec1f753
Jesse Mazzie <jessegalt@yahoo.com>
2019-11-30 12:06:56 -0500

    JS now runs at the appropriate time.

--- background.js
+++ background.js
@@ -1,3 +1,11 @@
 var elements = document.getElementsByClassName('dp__course__content__center__main__link box__item');
+console.log(elements);
+Array.from(elements).forEach(function(element) {
+	console.log(element.childNodes);
+})
 
+
+
+
+var elements = document.getElementsByClassName('dp__course__content__center__main__link box__item');
 console.log(elements);
